270a8372b4fac6abd91d681b71002e69df2f46ac
hajar <salame.hajar@gmail.com>
2026-03-04 15:41:08 +0100

    silver layer step 2 commits and repositories

---
+++ github_analytics/models/silver/stg_commits.sql
@@ -0,0 +1,32 @@
+-- models/silver/stg_commits.sql
+{{ config(
+    materialized='view'
+) }}
+with source as (
+    select * from {{ source('bronze', 'raw_commits') }}
+),
+cleaned as (
+    select 
+        --Rename sha to commit_sha, repo_full_name to repo_id.
+         sha as commit_sha,
+         repo_full_name as repo_id,
+         
+         -- Handle null author_login with COALESCE
+            coalesce(author_login, 'Unknown') as author_login,
+        
+        -- Cast author_date and committer_date to TIMESTAMP.
+        cast(author_date as timestamp) as author_date,
+        cast(committer_date as timestamp) as committer_date,
+    
+        --  Extract day_of_week and hour_of_day from author_date (useful for analyzing development patterns).
+        extract(dayofweek from author_date) as day_of_week,
+        extract(hour from author_date) as hour_of_day,
+
+        --  Truncate message to 200 characters.
+        left(message, 200) as message
+
+
+    from source
+    where sha is not null-- Filter rows where sha IS NULL.
+)
+select * from cleaned
---
+++ github_analytics/models/silver/stg_repositories.sql
@@ -0,0 +1,38 @@
+-- models/silver/stg_repositories.sql
+{{ config(
+    materialized='view'
+) }}
+
+with source as (
+    select * from {{ source('bronze', 'raw_repositories') }}
+),
+
+cleaned as (
+    
+    select
+        -- Rename full_name to repo_id (business key).
+        full_name as repo_id,
+
+        -- Cast dates (created_at, updated_at, pushed_at) to TIMESTAMP
+        cast(created_at as timestamp) as created_at,
+        cast(updated_at as timestamp) as updated_at,
+        cast(pushed_at as timestamp) as pushed_at,
+
+        -- Cast numeric metrics (stargazers_count, forks_count, etc.) to INTEGER.
+        cast(stargazers_count as integer) as stargazers_count,
+        cast(forks_count as integer) as forks_count,
+        cast(open_issues_count as integer) as open_issues_count,
+
+        -- Replace null values: description → ’No description’, language → ’Unknown’.
+        coalesce(description, 'No description') as description,
+        coalesce(language, 'Unknown') as language,
+
+        --  Compute a derived column repo_age_days (difference between today and created_at).
+        datediff(current_date, cast(created_at as date)) as repo_age_days,
+        
+    -- Exclude archived repositories (archived = true).
+    from source
+    where not archived 
+)
+
+select * from cleaned
